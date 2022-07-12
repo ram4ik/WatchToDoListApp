cf28efc4928bd825030979b9a75c8ed89721acbb
Ramill <ramill_ibragimov@icloud.com>
2022-07-12 16:33:49 +0300

    Initial Commit

---
+++ WatchToDoListApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchToDoListApp WatchKit Extension
+//
+//  Created by Ramill Ibragimov on 12.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchToDoListApp WatchKit Extension/WatchToDoListAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchToDoListAppApp.swift
+//  WatchToDoListApp WatchKit Extension
+//
+//  Created by Ramill Ibragimov on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WatchToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
